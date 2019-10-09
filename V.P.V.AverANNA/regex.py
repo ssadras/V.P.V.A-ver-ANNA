5061a21ff01713218d4fefa7f3e537dc757cb671
iliavar <52670140+iliavar@users.noreply.github.com>
2019-10-09 12:26:59 -0400

    class emotions added to queue

--- V.P.V.AverANNA/regex.py
+++ V.P.V.AverANNA/regex.py
@@ -49,3 +49,29 @@ def findall (inp,target):#### target = open|go
 
 def search (inp,text):
 	return True if text in inp else False ;
+
+class emotions (object):
+	def __init__ (self, text):
+		self.text = text
+	@staticmethod
+	def _freq_ (text):
+		splited = text.split()
+		for i in splited:
+			if i in "!@#$%^&*()_+<>?":
+				splited.remove(i)
+		allwords = [(splited[word], splited.count(splited[word]), word) for word in range(len(splited))]
+		
+		return allwords
+	def checkCore (self):
+		allwords = _freq_(self.text)
+		freqs = []
+		for word, ferq, index in allwords:
+			freqs.append([ferq,index])
+		Max = [0, 0]
+		for obj in freqs:
+			if obj[0]>Max[0]:
+				Max = obj[:]
+		return Max
+text = emotions ("hello world !")
+array = text._freq_()
+print(array)
