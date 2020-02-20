d1d7c353ee1440763e8eff4f8277ed4fb2e56569
ilia <ilia.var84@gmail.com>
2020-02-19 23:28:31 -0800

    solving equations not completed

--- ANNAEx/sciAnna/utiles.py
+++ ANNAEx/sciAnna/utiles.py
@@ -57,47 +57,115 @@ class Set (object) :
 			new_str += "%s"%(str(self.mems[i])+a)
 		string = "{%s}"%new_str
 		return string;
-
-class polynominal (object):
-	def __init__ (self, name,*coeffs):
-		self.coeffs = list(coeffs)
-		self.coeffs_set = Set(self.coeffs);
-		self.name = name;
-	def _create(self, x):
-		logical_result = 0
-		symbolised_result = ""
-		sym = ""
-		degree_result = []
-		for i in range(len(self.coeffs)):
-			sym = ""
-			if self.coeffs[i][0]:
-				sym = "+"
+class polynominal(object):
+	"""docstring for polynominal"""
+	def __init__(self, sentences):
+		self.sentences = sentences; # a sentence : (2, k)
+		#print(self.sentences)
+	def simplize (self):
+		print("simplization")
+		result = {}
+		p2 = []
+		degree_zero = []
+		var = None;
+		for sentence in self.sentences:
+			if sentence[-1] == "NaN":
+				degree_zero.append(sentence[0])
+				continue;
+			var = sentence[-1]
+			bool = 1
+			for key, val in result.items():
+				if var == key:
+					bool = 0
+			if bool:
+				result.update({var:sentence[0]})
 			else:
-				sym = "-"
-			logical_result += self.coeffs[i] * x ** (i+1);
-			symbolised_result += "+({}{}).{}^{}".format(sym, self.coeffs[i], x, i+1);
-			degree_result.append(i+1)
-		self.polynominal_logical_result = logical_result;
-		self.polynominal_symbolized_result = symbolised_result
-		self.degree_result = degree_result
-		return result;
+				result[var] += sentence[0];
+		for key, val in result.items():
+			p2.append((val, key))
+		for i in degree_zero:
+			p2.append((i, "NaN"))
+		self.simplized = p2[:]
+		print("p2:", p2)
+		#print("poly : ", polynominal(self.simplized).sentences)
+		return polynominal(self.simplized);
 
-	def __add__ (self, p2):
-		try:
-			logical_sum = self.polynominal_logical_result + p2.polynominal_logical_result
-		except :
-			logical_sum = "INF"
-			#assert True, "one of arguments dosen't have the parameter 'polynominal_logical_result'";
-			raise Exception("one of arguments haven't been created")
-		finally:
-			return logical_sum;
-	def __eq__ (self, p2):
-		try:
-			result = self.polynominal_logical_result == p2.polynominal_logical_result;
-		except :
-			result = False;
-			raise Exception("one of arguments haven't been created")
-		finally:
-			return result;
-	
+	def _mult (self, other_number):
+		mult = [(other_number*i[0], i[-1]) for i in self.sentences]
+		#print("mult: ", mult)
+		return polynominal(mult)
+
+	def __add__ (self, other):
+		new = self.sentences[:]
+		for sent in other.sentences:
+			new.append(sent);
+		return polynominal(new)
+
+	def coeffs (self):
+		return (x[0] for x in self.sentences);
+
+
+class equation (object):
+	''' p1 and p2 are polynominal objects'''
+	def __init__ (self, p1, p2):
+		self.p1 = p1
+		self.p2 = p2
+
+	def simplize_each_side(self):
+		return equation(self.p1.simplize(), self.p2.simplize())
+
+	def move (self):
+		#making polynominal
+		new_one = []
+		polynm = self.p2.simplize();
+		for coeff, var in polynm.sentences:
+			if var == "NaN":
+				continue;
+			new_one.append((-1*coeff, var))
+			polynm.sentences.remove((coeff, var));
+		return (p1+polynominal(new_one)).simplize()
+
+'''
+p2 = polynominal([(10, "x")])
+print(tuple(p2.coeffs()))'''
+
+class equation_system (object):
+	def __init__ (self, *equations):
+		self.equations = list(equations);
+
+	def simplize_each_equation (self):
+		result = []
+		for eq in self.equations:
+			result.append(eq.simplize_each_side());
+		self.symplized = simplized;
+		#return simplized;
+
+	def solve (self):
+		### moving and making one side zero ###
+		for eq in self.equations:
+			eq.move()
+
+		A = []
+		B = []
+		for eq in self.equations:
+			A.append(list(eq.p1.coeffs()))
+			B.append([0])
+		a = np.array(A);
+		inv_a = np.linalg.inv(A)
+		b = np.array(B)
+		X = np.linalg.inv(a).dot(b)
+		return X
+p1 = polynominal([(4, "x"), (3, "y")])
+p2 = polynominal([(20, "NaN")])
+
+e1 = equation(p1, p2)
+
+p3 = polynominal([(-5, "x"), (9, "y")])
+p4 = polynominal([(26, "NaN")])
+
+e2 = equation(p3, p4)
+
+a = equation_system(e1, e2)
+print(a.solve())
+
 
